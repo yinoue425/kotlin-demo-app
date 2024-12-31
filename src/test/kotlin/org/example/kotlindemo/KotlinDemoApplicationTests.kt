e7012a861ba5f4d7deac8fb75d43bc51e88b2f4f
yinoue425 <usuke.i.22@gmail.com>
2024-12-31 19:22:04 +0900

    initial commit

---
+++ src/test/kotlin/org/example/kotlindemo/KotlinDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.kotlindemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
